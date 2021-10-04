c551f1aae76d7a4b6402aaa52dae60889b0035cd
Ahmed.Alaa <ahmed.alaa.ezzt@gmail.com>
2021-10-04 07:34:04 +0200

    00026 - prepare

---
+++ script/main.js
@@ -0,0 +1,22 @@
+let colors = ["red", "blue", "yellow", "black", "white"];
+let ids = [100, 200, 300, 400];
+let cars = [
+  { id: 123, color: "red" },
+  { id: 456, color: "blue" },
+  { id: 789, color: "red" }
+];
+
+let list = [
+	["tiger", "cat", "bear", "bird"]
+];
+
+var array2 = [
+  "Banana", 
+  [ 
+    "Apples", 
+    ["Oranges"], 
+    "Blueberries"
+  ]
+];
+
+let animals = ["cat", "elephant" ,"bee", "deer"],

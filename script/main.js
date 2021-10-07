474be3fc437c8c357a6fc14511c82adc968cac0b
Ahmed.Alaa <ahmed.alaa.ezzt@gmail.com>
2021-10-07 12:04:53 +0200

    00102 - prepare demo

--- script/main.js
+++ script/main.js
@@ -1,6 +1,8 @@
-const array = [1, 2, 10, 16];
+const array = [1, 2, 10, 16, 5, 3];
 
-const double = [];
-array.forEach(num => double.push(num *2));
+const numsGreaterThan5 = [];
+array.forEach(num => {
+  if(num > 5) numsGreaterThan5.push(num)
+});
 
-console.log(double);
+console.log(numsGreaterThan5);

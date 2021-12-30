1a82867979232d06fad27d2fa8098f02a7f49a03
Ahmed Alaa <ahmed.alaa.ezzt@gmail.com>
2021-12-30 19:29:00 +0200

    00119 - remove unwanted subscriptions

--- script/main.js
+++ script/main.js
@@ -4,44 +4,17 @@ import * as GoalsAction from "./state-management/goals/goals.actions.js";
 import rootReducer from "./state-management/root-reducer.js";
 import { checker } from "./state-management/middlewares/checker.js";
 
-
-
-
-const store = Redux.createStore(rootReducer,Redux.applyMiddleware(checker));
+const store = Redux.createStore(rootReducer, Redux.applyMiddleware(checker));
 
 store.subscribe(() => {
-  const {todos , goals } = store.getState();
-  
-  console.log(store.getState());
+  const { todos, goals } = store.getState();
 
   document.getElementById("todos").innerHTML = "";
   document.getElementById("goals").innerHTML = "";
 
   goals.forEach((goal) => addGoalToDOM(goal));
   todos.forEach((todo) => addTodoToDOM(todo));
-
 });
-
-
-store.subscribe(() => {
-  
-  console.log("subscribe 2");
-  
-
-
-});
-
-store.subscribe(() => {
-  
-  console.log("subscribe 3");
-  
-
-
-});
-
-
-
-
 
 function addTodo() {
   const input = document.getElementById("todo");
@@ -66,10 +39,7 @@ function addGoal() {
     id: generateId(),
     name,
   };
-  store.dispatch(  GoalsAction.addGoalAction(goal))
-
-
-
+  store.dispatch(GoalsAction.addGoalAction(goal));
 }
 
 document.getElementById("todoBtn").addEventListener("click", addTodo);
@@ -94,7 +64,7 @@ function addTodoToDOM(todo) {
   node.style.textDecoration = todo.complete ? "line-through" : "none";
 
   node.addEventListener("click", () => {
-    store.dispatch(TodosAction.toggleTodoAction(todo.id))
+    store.dispatch(TodosAction.toggleTodoAction(todo.id));
   });
 
   document.getElementById("todos").appendChild(node);
@@ -104,14 +74,9 @@ function addGoalToDOM(goal) {
   const node = document.createElement("li");
   const text = document.createTextNode(goal.name);
   const removeBtn = createRemoveButton(() => {
-    store.dispatch(GoalsAction.removeGoalAction(goal.id))
-
-   
-
+    store.dispatch(GoalsAction.removeGoalAction(goal.id));
   });
   node.appendChild(text);
   node.appendChild(removeBtn);
   document.getElementById("goals").appendChild(node);
 }
-
-
